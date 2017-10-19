05f96c50ea8146f7bdd8fc00d6f1537c4bdd94da
corolyy <coroyu@163.com>
2017-10-19 23:08:35 +0800

    Sort: bubble sort

---
+++ algorithms/sort/bubble_sort.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+
+org_list = [3, 8, 11, 5, 7, 1, 8, 5]
+
+for i in range(1, len(org_list)):
+    for j in range(len(org_list) - i):
+        if org_list[j] > org_list[j + 1]:
+            org_list[j], org_list[j + 1] = org_list[j + 1], org_list[j]
+
+print org_list # --> [1, 3, 5, 7, 8]
